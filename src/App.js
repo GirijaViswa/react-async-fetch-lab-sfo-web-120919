fd73f13a3045f51b24d2a707041a04bdb2c64ec6
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-02-23 16:09:48 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, {Component} from "react";
+
+class App extends Component
+{
+    state = {data : []}
+    componentDidMount()
+    {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp => resp.json())
+        .then(data => this.setstate(data: data))
+    }
+    render()
+    {
+        return (
+            <div>
+            {this.state.data}
+            </div>
+        )
+    }
+}
+export default App;
